8e0c03cc17bcc0a62a4b8415b9db52a1ec955208
A-Dolah <a.dolah.dev@gmail.com>
2020-02-11 19:39:42 +0100

    finished project

--- theProject.js
+++ theProject.js
@@ -1,38 +1,86 @@
 // SPOTIFY API DATA
 // Fetch API - Promise based
 
-const coded = btoa(
-  "40321916cd0e44878a981cab0fc9d7f0:36c4adc0ada9408e8570b9957f17ae70"
-);
+const getAccessToken = () => {
+	const coded = btoa(
+		`40321916cd0e44878a981cab0fc9d7f0:${process.env.CLIENT_SECRET}`
+	);
 
-fetch("https://accounts.spotify.com/api/token", {
-  method: "POST",
-  headers: {
-    // 'Content-Type': 'application/json'
-    "Content-Type": "application/x-www-form-urlencoded",
-    Authorization:
-      "Basic NDAzMjE5MTZjZDBlNDQ4NzhhOTgxY2FiMGZjOWQ3ZjA6MzZjNGFkYzBhZGE5NDA4ZTg1NzBiOTk1N2YxN2FlNzA="
-  },
-  body: ["grant_type=client_credentials"]
-})
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    return data;
-  })
-  .then(formatted => {
-    fetchData(formatted.access_token);
-  });
+	fetch('https://accounts.spotify.com/api/token', {
+		method: 'POST',
+		headers: {
+			// 'Content-Type': 'application/json'
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Authorization': `Basic ${coded}`
+		},
+		body: ['grant_type=client_credentials']
+	})
+		.then(response => response.json())
+		.then(data => {
+			return data;
+		})
+		.then(formatted => {
+			fetchData(formatted.access_token);
+		});
+};
 
 const fetchData = accessToken => {
-  fetch("https://api.spotify.com/v1/search?q=michael+jackson&type=album", {
-    method: "GET",
-    headers: {
-      // 'Content-Type': 'application/json'
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization: `Bearer ${accessToken}`
-    }
-  })
-    .then(response => response.json())
-    .then(data => console.log(data));
+	fetch('https://api.spotify.com/v1/search?q=michael+jackson&type=album', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Authorization': `Bearer ${accessToken}`
+		}
+	})
+		.then(response => response.json())
+		.then(data => {
+			const uris = data.albums.items
+				.map(item => {
+					return item.uri;
+				})
+				.slice(0, 5);
+
+			const tasks = uris.map(uri =>
+				fetch(`https://api.spotify.com/v1/albums/${uri.split(':')[2]}`, {
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded',
+						'Authorization': `Bearer ${accessToken}`
+					}
+				})
+			);
+			return Promise.all(tasks);
+		})
+		.then(responses => {
+			return Promise.all(responses.map(response => response.json()));
+		})
+		.then(albums => {
+			return Promise.all(albums.map(album => fetch(album.images[0].url)));
+		})
+		.then(responses => {
+			return Promise.all(responses.map(response => response.blob()));
+		})
+		.then(blobs => {
+			return blobs.map(blob => URL.createObjectURL(blob));
+		})
+		.then(urls => {
+			return Promise.all(
+				urls.map(url => {
+					return new Promise(resolve => {
+						const image = new Image();
+						image.addEventListener('load', () => {
+							resolve(image);
+						});
+						image.src = url;
+					});
+				})
+			);
+		})
+		.then(images => {
+			images.forEach(image => {
+				document.body.appendChild(image);
+			});
+		});
 };
+
+getAccessToken();
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
-const path = require("path");
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  // entry: ["./callback.js", "./promises.js"],
-  entry: "./theProject.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js"
-  },
-  mode: "development",
-  watch: true
+	// entry: ["./callback.js", "./promises.js"],
+	entry: './theProject.js',
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	mode: 'development',
+	watch: true,
+	plugins: [new Dotenv()]
 };
